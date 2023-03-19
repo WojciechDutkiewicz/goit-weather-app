d8f7febf46c45cc904cc677ca1861150a1550248
Wojciech Dutkiewicz <dutwoj95@gmail.com>
2023-03-19 15:14:10 +0100

    ok

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const dataEl = document.getElementById('data');
 const searchElButton = document.getElementById('search');
 const searchInputEl = document.getElementById('search-input');
+const errorMessageEl = document.getElementById('error-message');
 function getWeatherData(city) {
+  errorMessageEl.textContent = '';
+  dataEl.textContent = '';
   fetch(
     `http://api.weatherapi.com/v1/current.json?key=6b85e57ffb74420b825185110231703&q=${city}&aqi=no`
   )
     .then(response => response.json())
     .then(data => {
-      dataEl.textContent = JSON.stringify(data, null, 2);
+      if (data.error) {
+        errorMessageEl.textContent = data.error.message;
+      } else {
+        dataEl.textContent = `Current temperature is ${data.current.temp_c}`;
+      }
     });
 }
 searchElButton.addEventListener('click', () => {
